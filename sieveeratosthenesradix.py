21e3ee7d15562a8b7f04bf690ee35ad52ea64471
Sean Smith <117996654+olsmithy1@users.noreply.github.com>
2024-03-19 21:00:10 +0000

    Update sieveeratosthenesradix.py
    Minor improvements and adjustments, the last upload was from a testing area and included a counting sort routine. This has been removed now.

--- sieveeratosthenesradix.py
+++ sieveeratosthenesradix.py
@@ -13,29 +13,15 @@ def sieve_eratosthenes_radix(n):
     # Using radix sort to iterate through numbers efficiently
     exp = 1
     while n // exp > 0:
-        # Counting sort to mark numbers as prime or not
-        count = [0] * 10
+        # Radix sort to mark numbers as prime or not
+        temp = [0] * (n + 1)
         for num in range(n + 1):
             digit = (num // exp) % 10
-            count[digit] += 1
-
-        for i in range(1, 10):
-            count[i] += count[i - 1]
-
-        temp = [0] * (n + 1)
-        i = n
-        while i >= 0:
-            digit = (i // exp) % 10
-            if is_prime[i] and digit != 0:
-                # i is prime, mark its multiples as not prime
-                for j in range(2 * i, n + 1, i):
+            if is_prime[num] and digit != 0:
+                # num is prime, mark its multiples as not prime
+                for j in range(2 * num, n + 1, num):
                     is_prime[j] = False
-            temp[count[digit] - 1] = i
-            count[digit] -= 1
-            i -= 1
-
-        for i in range(n + 1):
-            temp[i] = is_prime[temp[i]]  # Update temp based on is_prime
+            temp[num] = is_prime[num]
 
         for i in range(n + 1):
             is_prime[i] = temp[i]  # Copy the updated values back to is_prime
@@ -43,10 +29,11 @@ def sieve_eratosthenes_radix(n):
         exp *= 10
 
     # Return a list of nonprime numbers
-    nonprimes = [i for i in range(n + 1) if is_prime[i]]
+    nonprimes = [i for i in range(n + 1) if not is_prime[i]]
     return nonprimes
 
 if __name__ == "__main__":
     n = 200
     nonprime_list = sieve_eratosthenes_radix(n)
     print("Non Prime numbers up to", n, "are:", nonprime_list)
+    
